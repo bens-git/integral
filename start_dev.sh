fe87cacb1f4a0d0e1013f01c940a623cfc65b982
Ben Small <mail.ben.small@gmail.com>
2026-06-10 22:09:18 -0400

    fix vite config

--- start_dev.sh
+++ start_dev.sh
@@ -1,3 +1,3 @@
 
-pm2 start "php artisan serve --host=0.0.0.0 --port=8012" --name integral-api
+pm2 start "php artisan serve --host=0.0.0.0 --port=8010" --name integral-api
 pm2 start npm --name "integral" -- run dev
--- vite.config.js
+++ vite.config.js
@@ -1,46 +1,58 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: "resources/js/app.js",
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "VITE_");
+
+    return {
+        plugins: [
+            laravel({
+                input: "resources/js/app.js",
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+        ],
+
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "resources/js"),
+            },
+        },
+
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `@use "vuetify/styles" as *;`,
                 },
             },
-        }),
-    ],
-
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "resources/js"),
         },
-    },
 
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: `@use "vuetify/styles" as *;`,
+        server: {
+            host: env.VITE_SERVER_HOST,
+            port: parseInt(env.VITE_SERVER_PORT, 10),
+            strictPort: true,
+            hmr: {
+                host: env.VITE_HMR_HOST,
+                port: parseInt(env.VITE_HMR_PORT, 10),
             },
+            cors: true,
         },
-    },
 
-    server: {
-        host: true,
-        port: 8098,
-        strictPort: true,
-        hmr: {
-            host: "192.168.0.146",
-            port: 8098,
+        define: {
+            __APP_NAME__: JSON.stringify(env.VITE_APP_NAME),
+            __VITE_SERVER_HOST__: JSON.stringify(env.VITE_SERVER_HOST),
+            __VITE_SERVER_PORT__: parseInt(env.VITE_SERVER_PORT, 10),
+            __VITE_HMR_HOST__: JSON.stringify(env.VITE_HMR_HOST),
+            __VITE_HMR_PORT__: parseInt(env.VITE_HMR_PORT, 10),
         },
-        cors: true,
-    },
+    };
 });
