513cebbebe64c90d8b2e50c21ca42cc3ed03db0b
Ben Small <mail.ben.small@gmail.com>
2026-06-11 23:32:53 -0400

    fix csrf issue

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,7 +4,20 @@ import Pusher from 'pusher-js';
 
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]')?.content;
+window.axios.defaults.withCredentials = true;
+window.axios.defaults.headers.common = window.axios.defaults.headers.common || {};
+
+window.axios.interceptors.request.use((config) => {
+    const token = document.cookie
+        .split('; ')
+        .find((c) => c.startsWith('XSRF-TOKEN='))
+        ?.split('=')[1];
+    if (token) {
+        config.headers.common = config.headers.common || {};
+        config.headers.common['X-XSRF-TOKEN'] = decodeURIComponent(token);
+    }
+    return config;
+});
 
 const getMeta = (name) => document.querySelector(`meta[name="${name}"]`)?.content;
 
