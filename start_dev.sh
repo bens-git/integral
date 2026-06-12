1168f8ac56abea6a5f1f7e558283667ced090c9f
Ben Small <mail.ben.small@gmail.com>
2026-06-11 23:40:09 -0400

    ignore generated ziggue file

--- start_dev.sh
+++ start_dev.sh
@@ -4,7 +4,11 @@ REVERB_PORT=$(grep "^REVERB_PORT=" .env | cut -d= -f2)
 REVERB_HOST=$(grep "^REVERB_HOST=" .env | cut -d= -f2)
 REVERB_APP_KEY=$(grep "^REVERB_APP_KEY=" .env | cut -d= -f2)
 REVERB_APP_ID=$(grep "^REVERB_APP_ID=" .env | cut -d= -f2)
+APP_URL=$(grep "^APP_URL=" .env | cut -d= -f2)
+APP_URL=${APP_URL:-http://localhost:8010}
 
 pm2 start "php artisan serve --host=0.0.0.0 --port=8010" --name integral-api
 pm2 start npm --name "integral" -- run dev
 pm2 start "php artisan reverb:start --host=${REVERB_HOST} --port=${REVERB_PORT}" --name "integral-reverb"
+
+php artisan ziggy:generate --url="${APP_URL}"
