3e87c992b7937322884571c1624ff37a0a80e9f6
Ben Small <mail.ben.small@gmail.com>
2026-06-12 21:21:10 -0400

    remove unused seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,6 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
-        // Demo users for testing
-        $this->call(DemoUsersSeeder::class);
+      
     }
 }
